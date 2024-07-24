9e2ee14b2597aac638a43cb7d732add9f19edc56
jaya <iamjayamma2001@gmail.com>
2024-07-24 18:53:27 +0530

    Sample program using lambdafunction

---
+++ LambdaFunction.py
@@ -0,0 +1,10 @@
+double=lambda x:x*2
+print(double(6))
+multiply=lambda x,y:x*y
+print(multiply(2,3))
+add=lambda x,y,z:x+y+z
+print(add(13,45,34))
+full_name=lambda first_name, last_name:first_name+" "+last_name
+print(full_name("bro","code"))
+age_check=lambda age:True if age>=18 else False
+print(age_check(18))
